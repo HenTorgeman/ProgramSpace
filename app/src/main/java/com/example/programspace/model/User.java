1d7bf86f9c0e876df4039e867af3d89d4ea2043a
Hen Torgeman <hen.torgeman6@gmail.com>
2022-01-20 16:02:08 +0200

    Create User Model, Register and login activities

---
+++ app/src/main/java/com/example/programspace/model/User.java
@@ -0,0 +1,19 @@
+package com.example.programspace.model;
+
+public class User {
+
+    public String name,email,password,des;
+
+    public User() {
+
+    }
+
+    public User(String name, String email, String password, String des) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.des = des;
+    }
+
+
+}
